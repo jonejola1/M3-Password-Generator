296da60de059381a599122d8c497dae6b4a29a61
janji <62443314+jonejola1@users.noreply.github.com>
2023-03-21 14:03:45 +0100

    First Issue

--- Program.cs
+++ Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            
+            PromtUser();
+        }
+
+        static void PromtUser()
+        {
+            Console.WriteLine("Min. 1 lower case (l)\nMin. 1 upper case (L)\nMin. 2 special characters (s)\nMin. 2 digits (d)");
+            Console.Write("Skriv inn antall tegn: ");
+
+            var symbols = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Skriv inn krav: ");
+
+            string optionInput = Console.ReadLine();
+            foreach (char c in optionInput)
+            {
+                if (c != 'l' && c != 'L' && c != 's' && c != 'd')
+                    PromtUser();
+            }
+
+            Console.WriteLine(symbols + " " + optionInput);
+            Console.Read();
         }
     }
 }
