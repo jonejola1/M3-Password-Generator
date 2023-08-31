1db55212d77fc0e0e26be060a5c5d1766a04bb9f
janji <62443314+jonejola1@users.noreply.github.com>
2023-08-31 14:33:34 +0200

    a

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace M3_Password_Generator
+﻿using System.Text.RegularExpressions;
+
+namespace M3_Password_Generator
 {
     class Program
     {
@@ -6,13 +8,14 @@
         {
             PromtUser();
         }
-
+        static string optionInput = "";
+        static int symbols = 0;
         static void PromtUser()
         {
             Console.WriteLine("Min. 1 lower case (l)\nMin. 1 upper case (L)\nMin. 2 special characters (s)\nMin. 2 digits (d)");
             Console.Write("Skriv inn antall tegn: ");
 
-            var symbols = Convert.ToInt32(Console.ReadLine());
+            symbols = Convert.ToInt32(Console.ReadLine());
 
             Console.Write("Skriv inn krav: ");
 
@@ -24,7 +27,57 @@
             }
 
             Console.WriteLine(symbols + " " + optionInput);
-            Console.Read();
+            GeneratePassword();
+        }
+
+        static void GeneratePassword()
+        {
+            string pattern = "";
+            for (int i = 0; i < symbols; i++)
+            {
+                while (i < optionInput.Length)
+                {
+                    pattern += optionInput[i];
+                }
+                pattern += 'l';
+            }
+            Console.WriteLine(pattern);
+            Console.WriteLine(WriteRandomLowerCaseLetter());
+            WriteRandomDigits();
+        }
+
+        static char WriteRandomLowerCaseLetter()
+        {
+            Random rnd = new Random();
+            int randonumber = rnd.Next(0, 26);
+            char[] randomLowerCaseLetters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+            return randomLowerCaseLetters[randonumber];
+        }
+
+        static char WriteRandomUpperCaseLetter()
+        {
+            Random rnd = new Random();
+            int randonumber = rnd.Next(0, 26);
+            char[] randomUpperCaseLetters = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+            return randomUpperCaseLetters[randonumber];
+        }
+
+        static int WriteRandomDigits()
+        {
+            Random rnd = new Random();
+            int randomNumber = rnd.Next(0, 9+1);
+            return randomNumber;
+        }
+
+        static char WriteRandomSpecialCharacter()
+        {
+            Random rnd = new Random();
+            char[] randomSpecialCharacters = { '@', '!', '#', '%', '?', '$', '£' };
+
+            return randomSpecialCharacters[rnd.Next(0, randomSpecialCharacters.Length)];
         }
     }
+    
 }
